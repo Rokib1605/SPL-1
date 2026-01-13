fb18fc5fb92302c50b334a079cbb1af7dd7c998d
Rokib1605 <ikbalrokib029@gmail.com>
2026-01-13 23:53:04 +0600

    Added Member.h

--- Member.h
+++ Member.h
@@ -3,7 +3,6 @@
 
 #include <iostream>
 #include <cstring>
-using namespace std;
 
 class Member {
 public:
@@ -13,51 +12,17 @@ public:
     char phone[15];
     int booksBorrowed;
 
-    int borrowHistory[20]; 
+    int borrowHistory[20];
     int historyCount;
     double fineAmount;
-    
- 
 
-  
     
-    
-    Member() {
-        memberID = 0;
-        strcpy(name, "");
-        strcpy(email, "");
-        strcpy(phone, "");
-        booksBorrowed = 0;
-        historyCount = 0;
-        fineAmount = 0.0;
-        for (int i = 0; i < 20; i++) {
-            borrowHistory[i] = 0;
-        }
-    }
-    
-    // Display member info
-    void display() {
-        cout << "\n--- Member Details ---\n";
-        cout << "ID: " << memberID << endl;
-        cout << "Name: " << name << endl;
-        cout << "Email: " << email << endl;
-        cout << "Phone: " << phone << endl;
-        cout << "Books Borrowed: " << booksBorrowed << endl;
-        cout << "Fine Amount: Rs." << fineAmount << endl;
-    }
-    
+    Member();
 
-    bool canBorrow() {
-        return booksBorrowed < 5;  
-    }
-    
-    // Add book to history
-    void addToHistory(int bookID) {
-        if (historyCount < 20) {
-            borrowHistory[historyCount] = bookID;
-            historyCount++;
-        }
-    }
+    // Member functions
+    void display();
+    bool canBorrow();
+    void addToHistory(int bookID);
 };
 
 #endif
